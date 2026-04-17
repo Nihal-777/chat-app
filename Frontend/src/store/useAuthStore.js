fb0b7719f8b265ddd5caad47b5a47a61ab81d55b
Nihal1 <nihal42673787@gmail.com>
2026-04-17 23:39:41 +0530

    Configure separation for Vercel deployment

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://chat-app-6qtr.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
